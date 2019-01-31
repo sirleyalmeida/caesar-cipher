e85fd0686ecd52336230f739d692b228f29b0f48
sirleyalmeida <sirleyas90@gmail.com>
2019-01-31 16:40:17 -0200

    Readme versão 1, cipher com offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,23 @@
-function code(str, desloc) {
+let cipher = {};
+
+function cipher() {
+    let str = document.getElementById("msg").value;
+    let desloc = document.getElementById("deslocamento").value;
+    desloc = parseInt(desloc);
+    let encodif = encode(str, desloc);
+    let resultCodeFinal = document.getElementById("resultCode").innerHTML = "A mensagem codificada é: <br> " + encodif;
+}
+
+
+function descipher() {
+    let str = document.getElementById("msg").value;
+    let desloc = document.getElementById("deslocamento").value;
+    desloc = parseInt(desloc);
+    let decodif = decode(str, desloc);
+    let resultDecodeFinal = document.getElementById("resultDecode").innerHTML = "A mensagem decodificada é: <br> " + decodif;
+}
+
+cipher.encode = (str, desloc) => {
     let results = [];
 
     for (let i = 0; i < str.length; i++) {
@@ -26,15 +45,9 @@ function code(str, desloc) {
     return results.join("");
 }
 
-function cipher() {
-    let str = document.getElementById("msg").value;
-    let desloc = document.getElementById("deslocamento").value;
-    desloc = parseInt(desloc);
-    let codif = code(str, desloc);
-    let resultCodeFinal = document.getElementById("resultCode").innerHTML = "A mensagem codificada é: <br> " + codif;
-}
 
-function descode(str, desloc) {
+
+cipher.decode = (str, desloc) => {
     let results = [];
 
     for (let i = 0; i < str.length; i++) {
@@ -44,8 +57,7 @@ function descode(str, desloc) {
             desloc += 26
         }
         if (letter >= 65 && letter <= 90) {
-            // let calcDeslocMaius = ((letter - 90 - desloc) % 26)
-            // teste = 90 + calcDeslocMaius;
+
             let calcDeslocMaius = ((letter - 65 - (desloc % 26)) + 26) % 26 + 65
             let deslocFinal = String.fromCharCode(calcDeslocMaius);
             results.push(deslocFinal)
@@ -54,18 +66,11 @@ function descode(str, desloc) {
             let calcDeslocMinus = ((letter - 97 - (desloc % 26)) + 26) % 26 + 97
             let deslocFinal = String.fromCharCode(calcDeslocMinus);
             results.push(deslocFinal);
+
         } else {
             let othersLetters = String.fromCharCode(letter);
             results.push(othersLetters);
         }
     }
     return results.join("");
-}
-
-function descipher() {
-    let str = document.getElementById("msg").value;
-    let desloc = document.getElementById("deslocamento").value;
-    desloc = parseInt(desloc);
-    let descodif = descode(str, desloc);
-    let resultDescodeFinal = document.getElementById("resultDescode").innerHTML = "A mensagem descodificada é: <br> " + descodif;
 }
